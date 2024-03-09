3084f8ebfbd059396a7eeb786420d9fe0c3062a0
beyazkutupayisi <162023581+beyazkutupayisi@users.noreply.github.com>
2024-03-10 01:11:57 +0300

    Update Cansu.py

--- Cansu.py
+++ Cansu.py
@@ -1,1 +1,1 @@
-print('tool kapattim birdahaki tool icin lutfen bekleyin ve takipte kalin hkalin hepinizi seviyorum ')
+print('tool kapattim birdahaki tool icin lutfen bekleyin ve takipte kalin  hepinizi seviyorum ')
