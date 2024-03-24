26a23c0d7b35802c9864fd357d4f836accdfd018
beyazkutupayisi <162023581+beyazkutupayisi@users.noreply.github.com>
2024-03-25 00:14:22 +0300

    Update Thomas.py

--- Thomas.py
+++ Thomas.py
@@ -1,1 +1,19 @@
-
+from requests import get
+from random import choice
+from random import randrange
+from threading import Thread
+url='https://bad0c-403cd5ae3f06.herokuapp.com/Qredes/keyword='
+def randomword(length):
+  return ''.join(choice('azertyuiopqsdfghjklmwxcvbn1234567890') for i in range(length))
+def thomas():
+  while True:
+    try:
+       g=get(url+randomword(randrange(3,9))).json()['usernames']
+       for username in g:
+         print(username)
+         with open('qredesusers.txt','a') as f:
+           f.write(username+'\n')
+           f.close()
+    except:thomas()
+for _ in range(20):
+  Thread(target=thomas).start()
